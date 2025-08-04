d757ae114917aada91747975f7ede4faf829939c
AyushFulara <ayushfulara06@gmail.com>
2025-08-05 03:00:24 +0530

    Did count digits problem

--- Basic Maths/src/CountDigits.java
+++ Basic Maths/src/CountDigits.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class CountDigits {
 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter a number: ");
+        int number = in.nextInt();
+        System.out.println(countDigits(number));
+    }
+
+    public static int countDigits(int n) {
+        int count = 0;
+
+        while (n > 0) {
+            count++;
+            n /= 10;
+        }
+        return count;
+    }
 }

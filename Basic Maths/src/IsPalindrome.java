38e837ea9df0a1f213d6e992bd5f17597c2cc4d5
AyushFulara <ayushfulara06@gmail.com>
2025-08-05 03:49:35 +0530

    Did IsPalindrome problem

---
+++ Basic Maths/src/IsPalindrome.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class IsPalindrome {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter a number: ");
+        int number = in.nextInt();
+        System.out.println(isPalindrome(number));
+    }
+    public static boolean isPalindrome(int x) {
+        int original = x;
+        int reverseNumber = 0;
+
+        while (x > 0) {
+            int remainder = x % 10;
+            reverseNumber = reverseNumber * 10 + remainder;
+            x /= 10;
+        }
+
+        if (reverseNumber == original) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
